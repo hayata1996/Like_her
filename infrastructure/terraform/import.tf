9e2711dfcfc2e17d375c3d35e43507122cefaf8d
hayata1996 <kk19960603hh@gmail.com>
2025-04-27 22:13:33 +0700

    Add Terraform import configuration to handle existing resources

---
+++ infrastructure/terraform/import.tf
@@ -0,0 +1,9 @@
+import {
+  to = google_secret_manager_secret.gemini_api_key
+  id = "projects/433235895318/secrets/gemini-api-key"
+}
+
+import {
+  to = google_bigquery_dataset.like_her_dataset
+  id = "projects/433235895318/datasets/like_her_dataset"
+}
