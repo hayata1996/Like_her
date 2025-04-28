3c35ad8e763ade00e973274beaf0c544a73a6f43
hayata1996 <kk19960603hh@gmail.com>
2025-04-28 08:14:44 +0700

    Remove BigQuery imports for deleted resources

--- infrastructure/terraform/import.tf
+++ infrastructure/terraform/import.tf
@@ -2,18 +2,3 @@ import {
   to = google_secret_manager_secret.gemini_api_key
   id = "projects/433235895318/secrets/gemini-api-key"
 }
-
-import {
-  to = google_bigquery_dataset.like_her_dataset
-  id = "projects/433235895318/datasets/like_her_dataset"
-}
-
-import {
-  to = google_bigquery_table.health_data
-  id = "projects/433235895318/datasets/like_her_dataset/tables/health_data"
-}
-
-import {
-  to = google_bigquery_table.news_data
-  id = "projects/433235895318/datasets/like_her_dataset/tables/news_data"
-}
